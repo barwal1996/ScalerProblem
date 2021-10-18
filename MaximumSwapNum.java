886c0b3768b320ed3f72d06f871dc3fac058eedf
Amit Barwal <33135462+barwal1996@users.noreply.github.com>
2021-10-19 02:22:00 +0530

    Add files via upload

---
+++ MaximumSwapNum.java
@@ -0,0 +1,31 @@
+//import java.rmi.dgc.DGC;
+
+public class MaximumSwapNum {
+    public static void main(String[] args) {
+        int num=2736;
+        int res=maximumSwap(num);
+        System.out.println(res);
+    }
+    public static int maximumSwap(int num){
+
+        char[] digits = Integer.toString(num).toCharArray();
+        
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+        
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+        }
+        
+        return num;
+    }
+}
